5d7d84443cbdbc1b936d372075e985e73fd30ecc
muslimindh <19102238@ittelkom-pwt.ac.id>
2024-05-29 14:28:27 +0700

    fixed created at

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Transaction extends Model
 {
@@ -14,4 +15,9 @@ class Transaction extends Model
         'type',
         'category',
     ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone('Asia/Jakarta')->format('d-m-Y, H:i:s');
+    }
 }
